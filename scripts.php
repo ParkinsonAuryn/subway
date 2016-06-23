c97eb0f58ce4beb2e9c171c91e3aaa018d330fd5
John Sukie <admin@goldpvp.net>
2016-06-23 21:12:18 +0100

    release

---
+++ scripts.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * scripts.php
+ *
+ * Author: John Sukie
+ *
+ * Core Scripts used on all pages
+ *
+ */
+?>
+
+<!-- Core JS files -->
+<!-- <script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script> -->
+<!-- <script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script> -->
+
+<script type="text/javascript" src="assets/js/core/libraries/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/core/libraries/jquery_ui/interactions.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/loaders/blockui.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/ui/nicescroll.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/ui/drilldown.js"></script>
+<!-- <script type="text/javascript" src="assets/js/plugins/ui/prism.min.js"></script> -->
+<script type="text/javascript" src="assets/js/plugins/notifications/pnotify.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/notifications/sweet_alert.min.js"></script>
+<!-- /core JS files -->
+
+<!-- Theme JS files -->
+
+<script type="text/javascript" src="assets/js/plugins/forms/styling/switch.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/forms/styling/uniform.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/forms/styling/switchery.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/forms/inputs/passy.js"></script>
+<script type="text/javascript" src="assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
+<script type="text/javascript" src="assets/js/plugins/forms/validation/validate.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/forms/selects/select2.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/forms/wizards/stepy.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/ui/moment/moment.min.js"></script>
+<!-- <script type="text/javascript" src="assets/js/plugins/ui/fullcalendar/fullcalendar.min.js"></script> -->
+<!-- <script type="text/javascript" src="assets/js/plugins/ui/fullcalendar/lang/ar.js"></script> -->
+<!-- <script type="text/javascript" src="assets/js/plugins/pickers/daterangepicker.js"></script> -->
+<!-- <script type="text/javascript" src="assets/js/plugins/velocity/velocity.min.js"></script> -->
+<!-- <script type="text/javascript" src="assets/js/plugins/velocity/velocity.ui.min.js"></script> -->
+<!-- <script type="text/javascript" src="assets/js/plugins/buttons/spin.min.js"></script> -->
+<!-- <script type="text/javascript" src="assets/js/plugins/buttons/ladda.min.js"></script> -->
+
+<!-- <script type="text/javascript" src="assets/js/plugins/media/fancybox.min.js"></script> -->
+<script type="text/javascript" src="assets/js/core/libraries/jasny_bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/tables/datatables/datatables.min.js"></script>
+<script src="assets/js/clipboard.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/visualization/echarts/echarts.js"></script>
+<script type="text/javascript" src="assets/js/plugins/visualization/highcharts.js"></script>
+<!-- <script type="text/javascript" src="assets/js/pages/dashboard.js"></script> -->
+<!-- /theme JS files -->
+
+<!-- Extra JS Files -->
+<!-- <script src="assets/js/clipboard.min.js"></script> -->
+<script type="text/javascript" src="assets/js/core-scripts.js"></script>
+
+<!-- /Extra JS Files -->
+
+
+<script type="text/javascript" src="assets/js/core/app.js"></script>
