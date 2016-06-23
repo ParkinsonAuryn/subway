6459fd29a99e83c2a9e8038c2a5f652609834fbf
John Sukie <admin@goldpvp.net>
2016-06-23 21:14:20 +0100

    change
    lel!

--- scripts.php
+++ scripts.php
@@ -10,17 +10,16 @@
 ?>
 
 <!-- Core JS files -->
-<!-- <script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script> -->
-<!-- <script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script> -->
-
+<script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script>
+<script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/core/libraries/bootstrap.min.js"></script>
-<script type="text/javascript" src="assets/js/core/libraries/jquery_ui/interactions.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/loaders/blockui.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/ui/nicescroll.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/ui/drilldown.js"></script>
-<!-- <script type="text/javascript" src="assets/js/plugins/ui/prism.min.js"></script> -->
+<script type="text/javascript" src="assets/js/plugins/ui/prism.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/notifications/pnotify.min.js"></script>
-<script type="text/javascript" src="assets/js/plugins/notifications/sweet_alert.min.js"></script>
+<script type="text/javascript" src="assets/js/core/libraries/jasny_bootstrap.min.js"></script>
+
 <!-- /core JS files -->
 
 <!-- Theme JS files -->
@@ -30,24 +29,24 @@
 <script type="text/javascript" src="assets/js/plugins/forms/styling/switchery.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/forms/inputs/passy.js"></script>
 <script type="text/javascript" src="assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
-<script type="text/javascript" src="assets/js/plugins/forms/validation/validate.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/forms/selects/select2.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/forms/wizards/stepy.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/forms/validation/validate.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/ui/moment/moment.min.js"></script>
-<!-- <script type="text/javascript" src="assets/js/plugins/ui/fullcalendar/fullcalendar.min.js"></script> -->
-<!-- <script type="text/javascript" src="assets/js/plugins/ui/fullcalendar/lang/ar.js"></script> -->
-<!-- <script type="text/javascript" src="assets/js/plugins/pickers/daterangepicker.js"></script> -->
-<!-- <script type="text/javascript" src="assets/js/plugins/velocity/velocity.min.js"></script> -->
-<!-- <script type="text/javascript" src="assets/js/plugins/velocity/velocity.ui.min.js"></script> -->
-<!-- <script type="text/javascript" src="assets/js/plugins/buttons/spin.min.js"></script> -->
-<!-- <script type="text/javascript" src="assets/js/plugins/buttons/ladda.min.js"></script> -->
+<script type="text/javascript" src="assets/js/plugins/pickers/daterangepicker.js"></script>
+<script type="text/javascript" src="assets/js/plugins/velocity/velocity.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/velocity/velocity.ui.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/buttons/spin.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/buttons/ladda.min.js"></script>
 
-<!-- <script type="text/javascript" src="assets/js/plugins/media/fancybox.min.js"></script> -->
-<script type="text/javascript" src="assets/js/core/libraries/jasny_bootstrap.min.js"></script>
-<script type="text/javascript" src="assets/js/plugins/tables/datatables/datatables.min.js"></script>
 <script src="assets/js/clipboard.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/media/fancybox.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/tables/datatables/datatables.min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/visualization/echarts/echarts.js"></script>
-<script type="text/javascript" src="assets/js/plugins/visualization/highcharts.js"></script>
+<!-- <script type="text/javascript" src="assets/js/plugins/visualization/d3/d3.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/visualization/d3/d3_tooltip.js"></script>
+<script type="text/javascript" src="assets/js/plugins/visualization/sparkline.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/visualization/highcharts.js"></script> -->
 <!-- <script type="text/javascript" src="assets/js/pages/dashboard.js"></script> -->
 <!-- /theme JS files -->
 
@@ -59,3 +58,4 @@
 
 
 <script type="text/javascript" src="assets/js/core/app.js"></script>
+
