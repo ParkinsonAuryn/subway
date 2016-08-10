2f246b27c6a60cc8fd6c0f75e02f3d14ce813853
John Sukie <admin@goldpvp.net>
2016-08-10 03:31:42 +0100

    memes
    added

--- domain.php
+++ domain.php
@@ -1,7 +1,7 @@
-if (isset($_POST['addDomain']) && !empty($_POST['addDomain'])){
+if (isset($_POST['suspendDomain']) && !empty($_POST['suspendDomain'])){
 
 
-        $required_fields = array('domain', 'backend', 'port', 'userUsername', 'username', 'token', 'ip', 'useragent');
+        $required_fields = array('domain', 'backend', 'port', 'domainID', 'pid', 'comment', 'userUsername', 'username', 'token', 'ip', 'useragent');
         foreach ($_POST as $key=>$value) {
             if (empty($value) && in_array($key, $required_fields) === true){
                 $responseArray['status'] = 'error';
@@ -12,10 +12,13 @@ if (isset($_POST['addDomain']) && !empty($_POST['addDomain'])){
         if (!isset($responseArray['status'])){
             $userUsername = sanitize($_POST['userUsername'], $dbc);
             $userUID = user_id_from_username($userUsername, $dbc);
+            $userCID = clientIDFromUID($userUID);
             $username   =   sanitize($_POST['username'], $dbc);
             $domain = sanitize(trim($_POST['domain']), $dbc);
+            $domainID = sanitize(trim($_POST['domainID']), $dbc);
             $backend = sanitize(trim($_POST['backend']), $dbc);
             $port = sanitize(trim($_POST['port']), $dbc);
+            $comment = sanitize(trim($_POST['comment']), $dbc);
             $pid = sanitize(trim($_POST['pid']), $dbc);
             $token   =   sanitize(trim($_POST['token']), $dbc);
             $ip         =   sanitize($_POST['ip'], $dbc);
@@ -27,27 +30,25 @@ if (isset($_POST['addDomain']) && !empty($_POST['addDomain'])){
                 $responseArray['message'] = 'Please contact a staff member';
             }else if (!valid_pid($userUID, $pid)){
                 $responseArray['status'] = 'error';
-                $responseArray['message'] = 'Please contact a staff member [SPID3 Error(' . $pid . '{' . $userUID . '}]';
-            }else if (has_domain_added($userUID, $pid, $domain)){
+                $responseArray['message'] = 'Please contact a staff member [SPID3 Error' . $pid . ']';
+            }else if (!valid_domainID($userUID, $pid, $domain, $domainID)){
                 $responseArray['status'] = 'error';
-                $responseArray['message'] = 'Domain already added';
-            }else if(!validate_domain($domain)){
-                $responseArray['status'] = 'error';
-                $responseArray['message'] = 'Invalid domain name';
-            }else if (!validate_ip($backend)){
-                $responseArray['status'] = 'error';
-                $responseArray['message'] = 'Invalid IPv4 Address ['.$backend.']';
-            }else if (!validate_port($port)){
-                $responseArray['status'] = 'error';
-                $responseArray['message'] = 'Invalid Port';
+                $responseArray['message'] = 'Domain has not beenadded';
             }else {
-                log_service_action($userUID, $pid, "A Technician added the domain <span class=\"text-bold\" id=\"toolT\" data-html=\"true\" data-popup=\"tooltip\" data-original-title=\"".$tmp_string."\">".$domain."</span>", $ip, getTime());
-                if(!addDomain($userUID, $pid, $domain, $backend, $port)){
+                log_service_action($userUID, $pid, "A Technician suspended the domain <span class=\"text-bold\" id=\"toolT\" data-html=\"true\" data-popup=\"tooltip\" data-original-title=\"".$tmp_string."\">".$domain."</span>", $ip, getTime());
+                if(!suspendDomain($userUID, $pid, $domain, $backend, $port)){
                     $responseArray['status'] = 'error';
-                    $responseArray['message'] = "There was a problem adding the domain. Try again later.";
+                    $responseArray['message'] = "There was a problem suspending the domain. Try again later.";
                 }else{
-                    $responseArray['status'] = 'success';
-                    $responseArray['message'] = 'The domain has been added ['.$port.']';
+                    $testVar = addDomainComment($pid, $domainID, $comment, $tmp_user_id, $userUID, $userCID);
+                    if (!$testVar){
+                        $responseArray['status'] = 'error';
+                        $responseArray['message'] = "There was a problem adding the domain comment. Try again later [" . $testVar . "]";
+                    }else{
+                        $responseArray['status'] = 'success';
+                        $responseArray['message'] = 'The domain has been suspended';
+                    }
+                    
                 }
             }
         }
